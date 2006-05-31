609907c68e54b0112ce4c51baae80f5810c6860f
lfelipe <lfelipe@198a2572-fd14-0410-b268-bcdf0d6d3c9b>
2006-05-31 03:18:00 +0000

    Created a basic Main class to test the svn plugin :)

---
+++ trunk/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import decoder.*;
+
+public class Main {
+	Decoder decoder;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
